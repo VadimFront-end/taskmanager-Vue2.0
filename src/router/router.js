dcb1d0e30b4fab08a9926ab0891c4cb3379f2a24
adolf <vadimschool7@mail.ru>
2020-09-22 19:08:22 +0300

    commit

--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,7 @@ import tmLogin from '../components/tm-login'
 import tmRegistration from '../components/tm-registration'
 import tmChangePassword from '../components/tm-change-password'
 import tmMainPage from '../components/tm-main-page'
+import tmTasksList from '../components/tm-tasks-list'
 import store from '../vuex/store'
 Vue.use(Router);
 
@@ -29,6 +30,15 @@ let router=new Router({
             path: '/mainPage',
             name: 'mainPage',
             component: tmMainPage,
+            beforeEnter(to,from,next) {
+                console.log(store.state.user.user)
+                store.state.user.id ? next() : next('/');
+            }
+        },
+        {
+            path: '/tasksList',
+            name: 'TasksList',
+            component: tmTasksList,
             beforeEnter(to,from,next) {
                 console.log(store.state.user.user)
                 store.state.user.id ? next() : next('/');
