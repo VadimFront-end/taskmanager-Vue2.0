fdd6939fd8c509d85a2e3047b5f01de2bfb3dbec
adolf <vadimschool7@mail.ru>
2020-10-20 18:22:28 +0300

    commit

--- src/components/modal-window-task.vue
+++ src/components/modal-window-task.vue
@@ -389,7 +389,6 @@ export default {
         };
         if (Number.isNaN(this.task.id)) this.$store.dispatch('createNewTask', newTask);
         else {
-          console.log(1)
           this.$store.dispatch('editTask', this.task);
         }
         this.$store.commit('closeTaskWindow');
